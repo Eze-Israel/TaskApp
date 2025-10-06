0c7a45944d3b1a7347f7a2383ea9df29652afc9a
Eze-Israel <ezeisraeloluoma123@gmail.com>
2025-10-07 00:38:42 +0100

    fixing tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', 
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}", 
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        dark: "#0f172a",
-        light: "#f8fafc",
-      },
+      
     },
   },
   plugins: [],
